eae55ca605ac09fe4fa00053bc941449dc2ebfc3
biwenshuai <biwenshuai@genomics.cn>
2025-09-12 15:59:30 +0800

    feat(main):can remove in 20250912

--- app/api/v1/endpoints/multi_chat.py
+++ app/api/v1/endpoints/multi_chat.py
@@ -11,6 +11,7 @@ class MultiChatRequest(BaseModel):
     data_choose: Dict[str, Any]
     query_template: str
     conversation_id: str
+    need_plan: int
 
 class MultiChatResponse(BaseModel):
     code: int
@@ -27,24 +28,32 @@ async def multi_chat_endpoint(request: MultiChatRequest):
     data_choose=request.data_choose
     query_template=request.query_template
     conversation_id=request.conversation_id
+    need_plan=request.need_plan
 
     # print(request.input)
     
     try:
         # 调用图像推荐函数
-        planning_result = await multi_chat_with_api(json.dumps(data_choose),query_template,conversation_id)
-        
-        if planning_result is None:
-            raise HTTPException(
-                status_code=500,
-                detail="图像推荐失败"
+        if need_plan==0:
+            planning_result = await multi_chat_with_api(json.dumps(data_choose),query_template,conversation_id)
+            
+            if planning_result is None:
+                raise HTTPException(
+                    status_code=500,
+                    detail="图像推荐失败"
+                )
+            
+            return MultiChatResponse(
+                code=200,
+                message="Success",
+                planning_result=planning_result
             )
-        
-        return MultiChatResponse(
-            code=200,
-            message="Success",
-            planning_result=planning_result
-        )
+        else:
+            return MultiChatResponse(
+                code=200,
+                message="Success",
+                planning_result={"need_plan":1,"key_text":"直接调用planning生成"}
+            )         
     except HTTPException:
         raise
     except Exception as e:
